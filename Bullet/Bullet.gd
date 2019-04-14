92efd841b4fc39201e75dd1f4d29133d5b1c5e5c
D-Chenkov <ChenkovvoknehC@gmail.com>
2019-04-13 23:13:43 -0700

    Adding Gun and Bullet Functions
    +Gun
    +Bullet

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,1 +1,12 @@
-extends RigidBody2D
+extends RigidBody2D   
+var BulletTypes = [ [1, preload("res://icon.png") ] ] 
+func _on_Bullet_body_entered(body): 
+	print(body.is_in_group("Bullets"))
+	if body.is_in_group("Bullets"): 
+		queue_free() 
+func CreateType(BulletType, Angle):
+	 mass=BulletTypes[BulletType][0] 
+	 $Texture.texture=BulletTypes[BulletType][1] 
+	 linear_velocity =Angle
+ 
+ 
--- Gun/Gun.gd
+++ Gun/Gun.gd
@@ -1,13 +1,28 @@
-extends Node2D
-
+extends Node2D 
 enum GunTypes {LIGHT, MEDIUM, HEAVY}
 var type
-
-var rate : float
-var power : float
-
+var Bullet = preload("res://Bullet/Bullet.tscn")
+var delay:bool = false
+var rate : float=1
+var power : float=1
+var Time :float=0 
 func _ready():
-	pass
-
-func shoot():
-	print('boom!')
+	pass 
+func _process(delta):
+	if delay==true:
+		Time+=delta
+	if Time >= rate:
+		delay=false 
+func shoot(): 
+	if delay==false:
+		print('boom!')  
+		var bullet = Bullet.instance()  
+		get_parent().add_child(bullet)
+		bullet.position = position 
+		bullet.CreateType(0, Vector2(-456,-72))
+		delay=true
+	else:
+		print('No Boom :c')  
+func _input(event):
+	if Input.is_key_pressed(KEY_SPACE):
+		 shoot()
